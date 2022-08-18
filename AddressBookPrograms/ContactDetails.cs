72bb50ed4fc6cddff5de4a70d7c99967709be5d0
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-18 10:40:20 +0530

    [Rushikesh] UC1 Create contact

---
+++ AddressBookPrograms/ContactDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookPrograms
+{
+    internal class ContactDetails
+    {
+        string FirstName, LastName, Address, city, state;
+        int zip, phoneNumber;
+
+        public void Contacts(string FirstName, string LastName, string Address, string city, string state, int zip, int phoneNumber)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            Console.WriteLine("Your Contact Details are= ");
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.WriteLine("Address: " + Address);
+            Console.WriteLine("city: " + city);
+            Console.WriteLine("state: " + state);
+            Console.WriteLine("zip: " + zip);
+            Console.WriteLine("phoneNumber: " + phoneNumber);
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ AddressBookPrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AddressBookPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+          
+            ContactDetails contact1 = new ContactDetails();
+            contact1.Contacts("Rushi", "Dharmadhikari", "Latur", "Latur", "Maharashtra", 413512, 2000000101);
+         
+            ContactDetails contact2 = new ContactDetails();
+            contact2.Contacts("Rsd", "D", "Mumbai", "Bombay", "Maharashtra", 111111, 1000000021);
+        }
+    }
+}
