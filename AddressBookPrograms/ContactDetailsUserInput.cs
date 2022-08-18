238148c10dfdb2398d1e0cd6b7a05793e9e158b6
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-18 10:48:41 +0530

    [Rushikesh] UC2 Contact Details by taking User Input

---
+++ AddressBookPrograms/ContactDetailsUserInput.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookPrograms
+{
+    internal class ContactDetailsUserInput
+    {
+        string FirstName, LastName, Address, city, state;
+        int zip, phoneNumber;
+        public void ReaderContactDetails()
+        {
+            Console.WriteLine("To create contact add the required details");
+
+            Console.WriteLine("Enter Firstname");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter LastName");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address");
+            Address = Console.ReadLine();
+
+            Console.WriteLine("Enter city");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter state");
+            state = Console.ReadLine();
+
+            Console.WriteLine("Enter zip");
+            zip = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter phoneNumber");
+            phoneNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+        }
+        public void DisplayContactDetails()
+        {
+            Console.WriteLine("Your Contact details are as below= ");
+            Console.WriteLine("FirstName= " + FirstName);
+            Console.WriteLine("LastName= " + LastName);
+            Console.WriteLine("Address= " + Address);
+            Console.WriteLine("city= " + city);
+            Console.WriteLine("state= " + state);
+            Console.WriteLine("zip= " + zip);
+            Console.WriteLine("phoneNumber= " + phoneNumber);
+        }
+    }
+}
--- AddressBookPrograms/Program.cs
+++ AddressBookPrograms/Program.cs
@@ -5,12 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
-          
-            ContactDetails contact1 = new ContactDetails();
-            contact1.Contacts("Rushi", "Dharmadhikari", "Latur", "Latur", "Maharashtra", 413512, 2000000101);
-         
-            ContactDetails contact2 = new ContactDetails();
-            contact2.Contacts("Rsd", "D", "Mumbai", "Bombay", "Maharashtra", 111111, 1000000021);
+
+            //UC1
+
+            //ContactDetails contact1 = new ContactDetails();
+            //contact1.Contacts("Rushi", "Dharmadhikari", "Latur", "Latur", "Maharashtra", 413512, 2000000101);
+
+            //ContactDetails contact2 = new ContactDetails();
+            //contact2.Contacts("Rsd", "D", "Mumbai", "Bombay", "Maharashtra", 111111, 1000000021);
+
+            //UC2
+            ContactDetailsUserInput contactDetailsUserInput = new ContactDetailsUserInput();
+            contactDetailsUserInput.ReaderContactDetails();
+            contactDetailsUserInput.DisplayContactDetails();
         }
     }
 }
