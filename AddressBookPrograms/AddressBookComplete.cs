aa3bdd405cd02b2fb2bd8aa46d4222a20140d6e2
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-20 23:03:41 +0530

    [Rushikesh] AddressBook-Multiple-Contacts all functions

---
+++ AddressBookPrograms/AddressBookComplete.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookPrograms
+{
+    internal class AddressBookComplete
+    {
+        string FirstName, LastName, Address, city, state, email;
+        int zip; long phoneNumber;
+
+        public AddressBookComplete(string FirstName, string LastName, string Address, string city, string state, int zip, long phoneNumber, string email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        public void DisplayValues()
+        {
+            Console.WriteLine(" FirstName= " + FirstName + "\n LastName= " + LastName + " \n Address= " + Address + "\n city= " + city + "\n state= " + state + "\n zip= " + zip + "\n phoneNumber= " + phoneNumber + "\n email= " + email + "\n");
+        }
+
+        public void EditValues(int field)
+        {
+            if (field == 1)
+            {
+                Console.WriteLine("Enter corrected lastName");
+                this.LastName = Console.ReadLine();
+            }
+            else
+            if (field == 2)
+            {
+                Console.WriteLine("Enter corrected Address");
+                this.Address = Console.ReadLine();
+
+            }
+            else
+            if (field == 3)
+            {
+                Console.WriteLine("Enter corrected City");
+                this.city = Console.ReadLine();
+
+            }
+            else
+            if (field == 4)
+            {
+                Console.WriteLine("Enter corrected State");
+                this.state = Console.ReadLine();
+
+            }
+            else
+            if (field == 5)
+            {
+                Console.WriteLine("Enter corrected Zip");
+                this.zip = int.Parse(Console.ReadLine());
+            }
+            else
+            if (field == 6)
+            {
+                Console.WriteLine("Enter corrected PhoneNumber");
+                this.phoneNumber = long.Parse(Console.ReadLine());
+            }
+            else
+            {
+                if (field == 7)
+                {
+                    Console.WriteLine("Enter corrected email");
+                    this.email = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
--- AddressBookPrograms/Program.cs
+++ AddressBookPrograms/Program.cs
@@ -28,14 +28,141 @@
             //editContactDetail.edit();
 
             //UC4
-            DeleteContact deleteContact = new DeleteContact();
-            deleteContact.Contacts("Rushi", "Dharmadhikari", "Latur", "Latur", "Maharashtra", 413512, 2000000101);
-            deleteContact.DisplayContactDetails();
-            deleteContact.DeleteContacts();
-            Console.WriteLine("After delete details are");
-            deleteContact.DisplayContactDetails();
+            //DeleteContact deleteContact = new DeleteContact();
+            //deleteContact.Contacts("Rushi", "Dharmadhikari", "Latur", "Latur", "Maharashtra", 413512, 2000000101);
+            //deleteContact.DisplayContactDetails();
+            //deleteContact.DeleteContacts();
+            //Console.WriteLine("After delete details are");
+            //deleteContact.DisplayContactDetails();
+
+            // Complete address book
+
+            Dictionary<string, AddressBookComplete> AddressDictionary = new Dictionary<string, AddressBookComplete>();
+            bool a = true;
+            int count = 0;
+            while (a)
+            {
+                Console.WriteLine("To add details enter 1");
+
+                Console.WriteLine("To Edit  details enter 2");
+
+                Console.WriteLine("To Delete details enter 3");
+
+                Console.WriteLine("To Display details enter 4");
+
+                Console.WriteLine("To Exit  enter 5");
+
+                int Case = int.Parse(Console.ReadLine());
+
+                switch (Case)
+                {
+                    case 1:
+                        string FirstName, LastName, Address, city, state, email;
+                        int zip;
+                        long phoneNumber;
+
+                        Console.WriteLine("Enter FirstName");
+                        FirstName = Console.ReadLine();
+                        Console.WriteLine("Enter LastName");
+                        LastName = Console.ReadLine();
+                        Console.WriteLine("Enter Address");
+                        Address = Console.ReadLine();
+                        Console.WriteLine("Enter city");
+                        city = Console.ReadLine();
+                        Console.WriteLine("Enter state");
+                        state = Console.ReadLine();
+                        Console.WriteLine("Enter zip");
+                        zip = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter phoneNumber");
+                        phoneNumber = long.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter email");
+                        email = Console.ReadLine();
+
+                        AddressBookComplete addressBookComplete = new AddressBookComplete(FirstName, LastName, Address, city, state, zip, phoneNumber, email);
+
+                        AddressDictionary.Add(FirstName, addressBookComplete);
+                        count++;
+                        Console.WriteLine("+++++++++++Contact Added Successfully+++++++++++++");
+                        break;
+                    case 4:
+                        if (count > 0)
+                        {
+                            Console.WriteLine("If you want all contacs from AddressBook press 1 and for perticular key details press 2");
+                            int YesNo = int.Parse(Console.ReadLine());
+
+                            if (YesNo == 1)
+                            {
+                                Console.WriteLine("Contact Details are= ");
+
+                                foreach (KeyValuePair<string, AddressBookComplete> Dict in AddressDictionary)
+                                {
+                                    Dict.Value.DisplayValues();
+
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Enter firstname to print details");
+                                string key1 = Console.ReadLine();
 
 
+                                foreach (KeyValuePair<string, AddressBookComplete> Dict in AddressDictionary)
+                                {
+                                    if (Dict.Key == key1)
+                                    {
+                                        Console.WriteLine("Details for {0} is= " + key1);
+                                        Dict.Value.DisplayValues();
+                                    }
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("********No contact to display**********");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Firstname to edit contact details");
+
+                        string fname = (Console.ReadLine());
+                        Console.WriteLine("Enter following numbers to edit peryicular field");
+                        Console.WriteLine("1-LastName\n 2-Address \n 3-city\n 4-state \n 5-zip \n 6-phoneNum \n 7-email");
+                        int field = int.Parse(Console.ReadLine());
+
+                        foreach (var Dict in AddressDictionary)
+                        {
+                            if (Dict.Key == fname)
+                            {
+                                Console.WriteLine("Details for {0} is= " + fname);
+                                Dict.Value.EditValues(field);
+                            }
+                        }
+                        break;
+                    case 3:
+
+                        Console.WriteLine("Enter FirstNmae to Delete Contact");
+                        string fname1 = (Console.ReadLine());
+
+                        AddressDictionary.Remove(fname1);
+                        Console.WriteLine("----------Contact Deleted Successfully----------------");
+                        count--;
+                        break;
+
+                    case 5:
+                        a = false;
+                        Console.WriteLine("......Exited........");
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter correct Number");
+                        break;
+
+                }
+
+
+
+            }
 
         }
     }
